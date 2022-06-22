fc0bafc219dbaa14c7907585eaaa6212129a0586
Vuong Nguyen <nhokskt0907@gmail.com>
2022-06-22 11:38:19 +0700

    update UI

--- main.py
+++ main.py
@@ -1,33 +1,80 @@
 import os
 import urllib.request
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
 from werkzeug.utils import secure_filename
 from os.path import join, dirname, realpath
+from svd_compressor import *
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "super secret key"
 UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads/')
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+PROCESSED_FOLDER = 'static/processed'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+K = 0
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 	
+def getFile_extension(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower()
+
 @app.route('/')
 def upload_form():
 	return render_template('home.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def getIMG():
+	if not os.path.isdir(app.config['UPLOAD_FOLDER']):
+		os.mkdir(app.config['UPLOAD_FOLDER'])
+	else:
+		files = os.listdir(app.config['UPLOAD_FOLDER'])
+		for item in files:
+			os.remove(os.path.join(app.config['UPLOAD_FOLDER'], item))
+			
+	files = os.listdir(app.config['UPLOAD_FOLDER'])
+	if len(files) > 0:
+		for item in files:
+			os.remove(os.path.join(app.config['UPLOAD_FOLDER'], item))
+
+	if request.method == 'POST':
+		img = request.files['file']
+		img.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename)))
+
+	return svd__compressor()
 
 @app.route('/getslider', methods=['GET', 'POST'])
 def get_slider():
 	data = request.data
 	k = int(data)
 	print(k)
-	return "None"
+	return k
+
+@app.route('/svd', methods=['GET', 'POST'])
+def svd__compressor():
+	img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'] ,os.listdir(app.config['UPLOAD_FOLDER'])[0]))
+	input_image = np.asarray(img)
+	try:
+		compress = svd_compress(input_image, k=get_slider())
+	except:
+		compress = svd_compress(input_image, k=0)
+		
+	ext = getFile_extension(os.path.join(app.config['UPLOAD_FOLDER'] ,os.listdir(app.config['UPLOAD_FOLDER'])[0]))
+	if not os.path.isdir(PROCESSED_FOLDER):
+		os.mkdir(PROCESSED_FOLDER)
+	else:
+		files = os.listdir(PROCESSED_FOLDER)
+		for item in files:
+			os.remove(os.path.join(PROCESSED_FOLDER, item))
+			
+	compress.save(os.path.join(PROCESSED_FOLDER, f'result.{ext}'))
+	response = {
+                "data": [os.path.join(PROCESSED_FOLDER, f'result.{ext}'), img.size[0], img.size[1], compress.size[0] * compress.size[1]]
+            } # [img, w, h, number_of_pixel compressed img]
+	return jsonify(response), 200
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(port='8000')
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -23,18 +23,41 @@ button.onclick = () => {
 inputSlider.addEventListener('change', function(){
   // window.alert('changed');
   // console.log('On Sliderbar changes...');
-  const URL = '/getslider'
+  // const URL = '/getslider'
+  const URL = '/svd'
   const xhr = new XMLHttpRequest();
   sender = inputSlider.value
-  xhr.open('POST', URL);
+  xhr.open('POST', URL, true);
   xhr.send(sender);
+  xhr.onreadystatechange = function(e) {
+    e.preventDefault();
+    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+      const update =  new Date();
+      const data = JSON.parse(xhr.responseText).data;
+      document.querySelector('.img__box').innerHTML = `<img src="${data[0]}?v=${update.getTime()}" />`; // To update avoid using image from cache
+      document.querySelector('#img__size').innerHTML = `${data[1]} x ${data[2]}`;
+      document.querySelector('#num__pixel').innerHTML = `${data[1]*data[2]}`;
+      document.querySelector('#compressed__size').innerHTML = `${data[3]}`;
+      document.querySelector('#compression__ratio').innerHTML = `${(data[1]*data[2])/data[3]}`;
+    }
+  }
 })
+
+function sendIMG(file){
+  const URL = '/upload';
+  const xhr = new XMLHttpRequest();
+  const formData = new FormData();
+  formData.append('file', file);
+  xhr.open('POST', URL);
+  xhr.send(formData);
+}
 
 // when browse
 input.addEventListener('change', function () {
   file = this.files[0];
   dropArea.classList.add('active');
   displayFile();
+  sendIMG(file);
 });
 
 // when file is inside drag area
@@ -60,7 +83,7 @@ dropArea.addEventListener('drop', (event) => {
   uploadImg.style.display = "none"
   showRes.style.display = "block";
   showImg.style.display = "block";
-
+  sendIMG(file);
   // containerRes.style.height = h + 500;
 });
 
@@ -122,10 +145,23 @@ function displayFile() {
 inputSlider.oninput = (()=>{
   let value = inputSlider.value;
   slideValue.textContent = value;
-  slideValue.style.left = (value/5) + "%";
+  slideValue.style.left = (value/3) + "%";
   slideValue.classList.add("show");
 });
 
 inputSlider.onblur = (()=>{
   slideValue.classList.remove("show");
 });
+
+// const xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function(e) {
+//   e.preventDefault();
+//   window.alert(2);
+//   console.log(3);
+//   if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+//     const update =  new Date();
+//     const data = JSON.parse(xhr.responseText).data;
+//     // document.querySelector('.img__box').textContent = '';
+//     document.querySelector('.img__box').textContent = `<img src="${data}?v=${update.getTime()}" />`; // To update avoid using image from cache
+//   }
+// }
